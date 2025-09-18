b0e9b77789a5f3bfe50d0c964850059ff2d2c36e
Aaron Chandler <41349277+AaronChandler976@users.noreply.github.com.>
2025-09-17 17:47:03 -0700

    Add board component layout

---
+++ src/components/Board.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
+
+function Board() {
+    const [pokemonList, setPokemonList] = useState([]);
+
+    useEffect(() => {
+        function fetchPokemon() {};
+        setPokemonList(fetchPokemon());
+    }, [])
+
+    function clickCard() {}
+
+
+    return (
+        <div class="board">
+            {pokemonList.map((pokemon) => {
+                <Card pokemon={pokemon} clickCard={clickCard} />
+            })}
+        </div>
+    );
+}
+
+export default Board;
