ac93bbe7fc25d97843ae8f9bc7d730fb9c304ea4
Aaron Chandler <41349277+AaronChandler976@users.noreply.github.com.>
2025-09-17 19:12:23 -0700

    Add board sizes to game rules constants

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,23 +2,22 @@ import { useEffect, useState } from "react";
 import Card from "./Card";
 
 function Board() {
-    const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonList, setPokemonList] = useState([]);
 
-    useEffect(() => {
-        function fetchPokemon() {};
-        setPokemonList(fetchPokemon());
-    }, [])
+  useEffect(() => {
+    function fetchPokemon() {}
+    setPokemonList(fetchPokemon());
+  }, []);
 
-    function clickCard() {}
+  function clickCard() {}
 
-
-    return (
-        <div class="board">
-            {pokemonList.map((pokemon) => {
-                <Card pokemon={pokemon} clickCard={clickCard} />
-            })}
-        </div>
-    );
+  return (
+    <div class="board">
+      {pokemonList.map((pokemon) => {
+        <Card pokemon={pokemon} clickCard={clickCard} />;
+      })}
+    </div>
+  );
 }
 
 export default Board;
---
+++ src/utils/game-rules.js
@@ -0,0 +1,17 @@
+export const BoardSizes = {
+  SMALL: {
+    value: "sm",
+    name: "Small",
+    numCards: 3,
+  },
+  MEDIUM: {
+    value: "md",
+    name: "Medium",
+    numCards: 4,
+  },
+  LARGE: {
+    value: "lg",
+    name: "Large",
+    numCards: 5,
+  },
+};
